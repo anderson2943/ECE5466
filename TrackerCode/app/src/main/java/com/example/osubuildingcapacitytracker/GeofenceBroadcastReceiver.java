f996a55ae61d6fb60add9d668ed27f6db134d248
Jamey <weyenberg.4@osu.edu>
2020-11-01 16:38:07 -0500

    Added initial location services and geofences. Still need to add buildings.

---
+++ TrackerCode/app/src/main/java/com/example/osubuildingcapacitytracker/GeofenceBroadcastReceiver.java
@@ -0,0 +1,74 @@
+package com.example.osubuildingcapacitytracker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.List;
+
+public class GeofenceBroadcastReceiver extends BroadcastReceiver {
+
+    //private final Handler handler;
+
+    public GeofenceBroadcastReceiver(){
+        //Log.e("GeofenceBR: ", "Created GBR");
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            String errorMessage = GeofenceStatusCodes
+                    .getStatusCodeString(geofencingEvent.getErrorCode());
+            Log.e("GeofenceBR: ", errorMessage);
+            return;
+        }
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL ||
+                geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT ) {
+            Toast.makeText(context, "geofence triggered", Toast.LENGTH_SHORT).show();
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
+            // Get the transition details as a String.
+            for(Geofence geofence: triggeringGeofences){
+                String name = geofence.getRequestId();
+                if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL){
+                    //updateText(name);
+                    //geofenceText.setText(name);'
+                    Intent textIntent = new Intent("UPDATE_UI");
+                    textIntent.putExtra("loc", name);
+                    context.sendBroadcast(textIntent);
+                    Log.d("GeofenceBR: ", name+" dwell transition");
+                }else{
+                    //updateText("not inside a geofence currently");
+                    Intent textIntent = new Intent("UPDATE_UI");
+                    textIntent.putExtra("loc", "Not currently inside a geofence");
+                    context.sendBroadcast(textIntent);
+                    Log.d("GeofenceBR: ", name+" exit transition");
+                }
+            }
+
+            // Send notification and log the transition details.
+            //sendNotification(geofenceTransitionDetails);
+            //
+        } else if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER){
+            // Log it, but don't do anything else
+            Log.e("GeofenceBR: ", "Entering geofence");
+        }
+
+    }
+
+
+
+}
--- TrackerCode/app/src/main/java/com/example/osubuildingcapacitytracker/ItemListActivity.java
+++ TrackerCode/app/src/main/java/com/example/osubuildingcapacitytracker/ItemListActivity.java
@@ -1,25 +1,46 @@
 package com.example.osubuildingcapacitytracker;
 
+import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.osubuildingcapacitytracker.dummy.DummyContent;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * An activity representing a list of Items. This activity
@@ -36,6 +57,23 @@ public class ItemListActivity extends AppCompatActivity {
      * device.
      */
     private boolean mTwoPane;
+    private double latitude;
+    private double longitude;
+    private double accuracy;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private final int MY_PERMISSION_REQ_FINE_LOC = 101;
+    private final int GEOFENCE_EXPIRE = 60000 * 6; //in ms, so 5 mins
+    private final int THREAD_SLEEP = 60000 * 5; //in ms, so 6 mins
+    private final int MY_REQ_CODE = 42;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private boolean updatesOn;
+    private GeofencingClient geofencingClient;
+    private PendingIntent geofencePendingIntent;
+    ArrayList<Geofence> geofenceList;
+    private Map<String, Double[]> landmarks;
+    private Thread getCloseLandmarks;
+    private boolean geofenceClientStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +104,239 @@ public class ItemListActivity extends AppCompatActivity {
         View recyclerView = findViewById(R.id.item_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
+
+        geofenceClientStarted = false;
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(7500); //in ms
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        //initialize location client
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        accuracy = location.getAccuracy();
+                    }
+                }
+            });
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_REQ_FINE_LOC);
+            }
+        }
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Location location = locationResult.getLastLocation();
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                accuracy = location.getAccuracy();
+            }
+        };
+
+        //populate landmarks list
+        //TODO replace this with the real landmarks
+        landmarks = new HashMap<>();
+        Double[] pool = {39.98839498, -83.02861959};
+        landmarks.put("pool", pool);
+        //okay so here we need to populate the geofenceList. I want to do this in a new thread that we can put to sleep and wake up
+        getCloseLandmarks = new Thread() {
+            @Override
+            public void run() {
+                ArrayList<Geofence> tempList = new ArrayList<>();
+                for (Map.Entry<String, Double[]> entry : landmarks.entrySet()) {
+                    Double[] latlng = entry.getValue();
+                    //check if building is within 1000 ft of user
+                    if (distance(latitude, latlng[0], longitude, latlng[1], 0.0, 0.0) < 300) {
+                        Geofence temp = new Geofence.Builder()// Set the request ID of the geofence. This is a string to identify this
+                                // geofence.
+                                .setRequestId(entry.getKey())
+
+                                // Set the circular region of this geofence.
+                                .setCircularRegion(
+                                        latlng[0], latlng[1], 80
+                                )
+
+                                // Set the expiration duration of the geofence
+                                .setExpirationDuration(GEOFENCE_EXPIRE)
+
+                                // Set the transition types of interest
+                                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_DWELL |
+                                        Geofence.GEOFENCE_TRANSITION_EXIT | Geofence.GEOFENCE_TRANSITION_ENTER)
+                                //set loitering delay in ms
+                                //TODO change this to a real value, set to 10s for testing
+                                .setLoiteringDelay(10000)
+                                // Create the geofence.
+                                .build();
+                        tempList.add(temp);
+                    }
+                }//end of for loop
+                synchronized (this) {
+                    geofenceList.clear(); //clear the list and then populate it again. This could make a race condition so sync it
+                    geofenceList.addAll(tempList);
+                    updateGeofences();
+                }//end of sync
+                try {
+                    // thread to sleep for 5 minutes. reminder that geofences expire after 6 minutes
+                    Thread.sleep(THREAD_SLEEP);
+                } catch (Exception e) {
+                    Log.e("AddGeofenceThread", e.toString());
+                }
+            }
+            /**
+             * Calculate distance between two points in latitude and longitude taking
+             * into account height difference. If you are not interested in height
+             * difference pass 0.0. Uses Haversine method as its base.
+             *
+             * lat1, lon1 Start point lat2, lon2 End point el1 Start altitude in meters
+             * el2 End altitude in meters
+             * @returns Distance in Meters
+             */
+            public double distance(double lat1, double lat2, double lon1,
+                                   double lon2, double el1, double el2) {
+
+                final int R = 6371; // Radius of the earth
+
+                double latDistance = Math.toRadians(lat2 - lat1);
+                double lonDistance = Math.toRadians(lon2 - lon1);
+                double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                        + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                        * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+                double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                double distance = R * c * 1000; // convert to meters
+
+                double height = el1 - el2;
+
+                distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+                return Math.sqrt(distance);
+            }
+        }; //end of thread declaration
+        //start the thread
+        getCloseLandmarks.start();
+
+        //initialize geofenceing client
+        geofencingClient = LocationServices.getGeofencingClient(this);
+        startLocationUpdates();
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (geofencePendingIntent != null) {
+            return geofencePendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceBroadcastReceiver.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        geofencePendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+        return geofencePendingIntent;
+    }
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_DWELL | GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(geofenceList);
+        return builder.build();
+    }
+
+    private PendingIntent getPendingBackgroundIntent(){
+        Intent intent = new Intent(this, LocationBackgroundManager.class);
+        intent.setAction(LocationBackgroundManager.ACTION_PROCESS_UPDATES);
+        return PendingIntent.getBroadcast(this, MY_REQ_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSION_REQ_FINE_LOC:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //we're all good
+                } else {
+                    Toast.makeText(getApplicationContext(), "This requires permissions to be granted", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    public void updateGeofences(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+            if(!geofenceClientStarted) {
+                geofencingClient = LocationServices.getGeofencingClient(this);
+                geofenceClientStarted = true;
+            }
+            // I think because there's the FLAG_UPDATE_CURRENT on the pending intent, it will just update fences if they already exist
+            //TODO get rid of the success listeners
+            geofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent())
+                    .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            // Geofences added, print Log message
+                            Log.d("Geofence: ", "geofence added!");
+                        }
+                    })
+                    .addOnFailureListener(this, new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Failed to add geofences, print log message
+                            Log.d("Geofence: ", "geofence failed");
+                        }
+                    });
+        }else{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, MY_PERMISSION_REQ_FINE_LOC);
+            }
+        }
+
+
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        }else{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_REQ_FINE_LOC);
+            }
+        }
+        //added in a check to see if client has been started already
+        updateGeofences();
+        //print
+        Log.d("StartUpdates: ", "Updates started");
+    }
+
+    private void startLocationUpdatesInBackground() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationRequest newlocationRequest = new LocationRequest();
+            newlocationRequest.setInterval(60000); //in ms
+            newlocationRequest.setFastestInterval(30000);
+            newlocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+            fusedLocationProviderClient.requestLocationUpdates(newlocationRequest, getPendingBackgroundIntent());
+        }else{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_REQ_FINE_LOC);
+            }
+        }
+        //print
+        Log.d("StartUpdatesBackgrnd: ", "Updates started");
+    }
+
+    private void stopLocationUpdatesInBackground(){
+        fusedLocationProviderClient.removeLocationUpdates(getPendingBackgroundIntent());
+        Log.d("StopUpdatesBackground: ", "Updates halted");
+    }
+
+    private void stopLocationUpdates(){
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        Log.d("StopUpdates: ", "Updates halted");
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
